f9de50bd4b4e9d677b1cb36e597be23195045181
ApramPepo <pepoapram2016@gmail.com>
2026-07-04 16:59:06 +0300

    debugged and tested for use

--- WinBat/water.bat
+++ WinBat/water.bat
@@ -19,14 +19,14 @@ if "%userchoice%" == "3" exit
 
 :start_reminder
 cls
+echo Setting up reminder
+echo msgbox "Time to drink water and stay hydrated!", 64, "Water Reminder" > "%temp%\water_msg.vbs"
+schtasks /create /tn "WaterReminderTask" /tr "wscript.exe \"%temp%\water_msg.vbs\"" /sc minute /mo 20 /f >nul 2>&1
 echo Reminder is now running in the background
 echo You can close this window
 echo To disable later, re-open this file and select option [2].
-schtasks /create /tn "WaterReminderTask" /tr "powershell -WindowStyle Hidden -Command [System.Windows.Forms.MessageBox]::Show('Time to drink water and stay hydrated!', 'Water Reminder', 'OK', 'Information')" /sc minute /mo 20 /f >nul 2>&1
 
 :: feel free to adjust "mo 20" for your prefered time in minutes.
-
-echo Task created successfully
 pause
 goto menu
 
@@ -38,3 +38,6 @@ echo  Reminder has been successfully removed.
 echo =========================================
 pause
 goto menu
+
+::Has been tested on both Win11 & 10. worked originally on 11 but not 10 during testing. it is now operational and should work on both
+::Has been tested on Windows 11 22H2 and Windows 10 22H2.
